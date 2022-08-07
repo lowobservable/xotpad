026cf86dcae3b2891045adc3ea2442978ac8cf7c
Andrew Kay <git@ajk.me>
2022-08-07 13:35:25 -0500

    start thread for incoming host PAD connection

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use clap::{Arg, ArgMatches, Command as ClapCommand};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use pty_process::Command as _;
 use std::str::FromStr;
+use std::sync::Arc;
 use tokio::io::{split, stdin, stdout};
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
 use tokio::process::Command;
 use tokio_util::codec::Framed;
 
@@ -82,14 +83,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
 
     if matches.is_present("listen") {
-        run_host_pad(&x25_parameters, &matches).await
+        run_host_pad(x25_parameters, &matches).await
     } else {
-        run_user_pad(&x25_parameters, &matches).await
+        run_user_pad(x25_parameters, &matches).await
     }
 }
 
 async fn run_user_pad(
-    x25_parameters: &X25Parameters,
+    x25_parameters: X25Parameters,
     matches: &ArgMatches,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let address = matches
@@ -132,7 +133,7 @@ async fn run_user_pad(
     let mut pad = UserPad::new(
         stdin(),
         stdout(),
-        x25_parameters,
+        &x25_parameters,
         &address,
         &outgoing,
         listener,
@@ -157,57 +158,79 @@ async fn run_user_pad(
 }
 
 async fn run_host_pad(
-    x25_parameters: &X25Parameters,
+    x25_parameters: X25Parameters,
     _matches: &ArgMatches,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let mut incoming = IncomingTable::new();
 
     incoming.add("^737202..$", "/home/andrew/tmp/inf0.py".into())?;
 
+    let incoming = Arc::new(incoming);
+    let x25_parameters = Arc::new(x25_parameters);
+
     let listener = TcpListener::bind(("0.0.0.0", xot::TCP_PORT)).await?;
 
     loop {
         let (tcp_stream, tcp_address) = listener.accept().await?;
 
-        println!("got a connection from {}", tcp_address);
+        println!("got a connection from {}, starting thread...", tcp_address);
 
-        let link = Framed::new(tcp_stream, XotCodec::new());
+        let incoming = Arc::clone(&incoming);
+        let x25_parameters = Arc::clone(&x25_parameters);
 
-        let (mut circuit, call_request) =
-            X25VirtualCircuit::wait_for_call(link, x25_parameters).await?;
-
-        let command = incoming.lookup(&call_request);
-
-        if command.is_none() {
-            circuit.clear_call(0, Some(0)).await?;
-            continue;
-        }
-
-        let command = command.unwrap();
-
-        let command: Vec<&str> = command.split_whitespace().collect();
-
-        let mut child = Command::new(command[0])
-            .args(&command[1..])
-            .spawn_pty(None)?;
-
-        let (a, b) = split(child.pty_mut());
-
-        circuit.accept_call().await?;
-
-        let pad = HostPad::new(a, b, circuit);
-
-        pad.run().await?;
-
-        // If we get here and the child process is still running... kill it!
-        if let Err(e) = child.kill().await {
-            println!("I tried killing, but got {:?}", e);
-        }
-
-        let exit_status = child.wait().await?;
-
-        println!("think we are done, exited with {:?}", exit_status);
+        tokio::spawn(async move {
+            if let Err(error) =
+                handle_host_pad_connection(tcp_stream, &incoming, &x25_parameters).await
+            {
+                println!("Something went wrong with {:?}", error);
+            }
+        });
     }
+}
+
+async fn handle_host_pad_connection(
+    tcp_stream: TcpStream,
+    incoming: &IncomingTable,
+    x25_parameters: &X25Parameters,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let link = Framed::new(tcp_stream, XotCodec::new());
+
+    let (mut circuit, call_request) =
+        X25VirtualCircuit::wait_for_call(link, x25_parameters).await?;
+
+    let command = incoming.lookup(&call_request);
+
+    if command.is_none() {
+        circuit.clear_call(0, Some(0)).await?;
+        return Ok(());
+    }
+
+    let command = command.unwrap();
+
+    let command: Vec<&str> = command.split_whitespace().collect();
+
+    let mut child = Command::new(command[0])
+        .args(&command[1..])
+        .spawn_pty(None)?;
+
+    let (a, b) = split(child.pty_mut());
+
+    circuit.accept_call().await?;
+
+    let pad = HostPad::new(a, b, circuit);
+
+    pad.run().await?;
+
+    // If we get here and the child process is still running... kill it!
+    if let Err(e) = child.kill().await {
+        println!("I tried killing, but got {:?}", e);
+    }
+
+    let exit_status = child.wait().await?;
+
+    println!("think we are done, exited with {:?}", exit_status);
+
+    Ok(())
 }
 
 fn get_x25_profile(name: &str) -> Option<X25Parameters> {
