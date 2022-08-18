eb3dca2bea22c4ad91a05debdb79b763a2e953bd
Andrew Kay <git@ajk.me>
2022-08-17 20:32:55 -0500

    cleanup

--- src/x25/packet.rs
+++ src/x25/packet.rs
@@ -1,5 +1,10 @@
+//! X.25 packets.
+
+/// Minimum X.25 packet length.
 pub const MIN_PACKET_LEN: usize = 3;
 
-// Maximum packet length based on a super extended header (7 bytes) and maximum
-// data field length (4096 bytes).
+/// Maximum X.25 packet length.
+///
+/// This length is based on a super extended header (7 bytes) and maximum data
+/// field length (4096 bytes).
 pub const MAX_PACKET_LEN: usize = 7 + 4096;
