abfb5824c89bdc84ab0148b1e207cd98dd266d28
Andrew Kay <git@ajk.me>
2022-08-18 08:32:23 -0500

    x

--- src/x121.rs
+++ src/x121.rs
@@ -6,12 +6,12 @@ use std::fmt;
 use std::str::FromStr;
 
 /// X.121 address.
-pub struct X121Address {
-    address: String,
+pub struct X121Addr {
+    addr: String,
 }
 
-impl X121Address {
-    /// Creates a new `X121Address` from digits.
+impl X121Addr {
+    /// Creates a new `X121Addr` from digits.
     pub fn from_digits<I: IntoIterator<Item = u8>>(digits: I) -> Result<Self, String> {
         let digits: Vec<u8> = digits.into_iter().collect();
 
@@ -19,34 +19,33 @@ impl X121Address {
             return Err("too many digits".into());
         }
 
-        // TODO: are leading zeros valid?
         if !digits.iter().all(|&d| d <= 9) {
             return Err("digits must be between 0 and 9".into());
         }
 
-        let address: String = digits.iter().map(|d| d.to_string()).collect();
+        let addr: String = digits.iter().map(|d| d.to_string()).collect();
 
-        Ok(Self { address })
+        Self::from_str(&addr)
     }
 
     /// Returns `true` if the address has no digits, and `false` otherwise.
     pub fn is_null(&self) -> bool {
-        self.address.is_empty()
+        self.addr.is_empty()
     }
 
     /// Returns an iterator over the digits.
     pub fn digits(&self) -> impl Iterator<Item = u8> + '_ {
-        self.address.chars().map(|c| c.to_digit(10).unwrap() as u8)
+        self.addr.chars().map(|c| c.to_digit(10).unwrap() as u8)
     }
 }
 
-impl fmt::Display for X121Address {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        self.address.fmt(f)
+impl fmt::Display for X121Addr {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.addr.fmt(fmt)
     }
 }
 
-impl FromStr for X121Address {
+impl FromStr for X121Addr {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, String> {
@@ -54,10 +53,18 @@ impl FromStr for X121Address {
             return Err("too long".into());
         }
 
+        // TODO: are leading zeros valid?
         if !s.chars().all(|c| c.is_ascii_digit()) {
             return Err("all characters must be digits between 0 and 9".into());
         }
 
-        Ok(Self { address: s.into() })
+        Ok(Self { addr: s.into() })
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // ...
 }
