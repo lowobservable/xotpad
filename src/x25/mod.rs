05262ded3fe504d623ec8d5369d45b78dbd59030
Andrew Kay <git@ajk.me>
2022-08-19 11:54:21 -0500

    add X25Modulo

--- src/x25/mod.rs
+++ src/x25/mod.rs
@@ -1,4 +1,8 @@
 //! X.25 packet switched networking.
+//!
+//! The X.25 protocol is specified in [ITU-T Rec. X.25 (10/96)].
+//!
+//! [ITU-T Rec. X.25 (10/96)]: https://www.itu.int/rec/T-REC-X.25-199610-I
 
 pub mod packet;
 
--- src/x25/packet.rs
+++ src/x25/packet.rs
@@ -1,4 +1,6 @@
 //! X.25 packets.
+//!
+//! This module provides functionality for encoding and decoding X.25 packets.
 
 /// Minimum X.25 packet length.
 pub const MIN_PACKET_LEN: usize = 3;
@@ -8,3 +10,21 @@ pub const MIN_PACKET_LEN: usize = 3;
 /// This length is based on a super extended header (7 bytes) and maximum data
 /// field length (4096 bytes).
 pub const MAX_PACKET_LEN: usize = 7 + 4096;
+
+/// Maximum X.25 channel number.
+pub const MAX_CHANNEL: u16 = 4095;
+
+/// X.25 packet sequence numbering scheme.
+///
+/// The sequence numbering scheme specifies the range of sequence numbers, and
+/// in some cases the packet format as a result.
+///
+/// Only normal and extended schemes are currently supported, super extended is
+/// not supported.
+pub enum X25Modulo {
+    /// Numbers cycle through the entire range 0 to 7.
+    Normal = 8,
+
+    /// Numbers cycle through the entire range 0 to 127.
+    Extended = 128,
+}
